2774a66df29f7267795ee37c9fc7790633c075a9
Filip Staffa <294522+fstaffa@users.noreply.github.com>
2022-10-09 21:59:28 +0200

    fix lsp + shellcheck in flycheck in sh-mode

--- config.el
+++ config.el
@@ -283,5 +283,17 @@
 
 (setq +format-with-lsp nil)
 
-(after! (:and flycheck lsp)
-   (flycheck-add-next-checker 'sh-shellcheck 'lsp))
+
+;; fix for lsp
+(defvar-local my/flycheck-local-cache nil)
+
+(defun my/flycheck-checker-get (fn checker property)
+  (or (alist-get property (alist-get checker my/flycheck-local-cache))
+      (funcall fn checker property)))
+
+(advice-add 'flycheck-checker-get :around 'my/flycheck-checker-get)
+
+(add-hook 'lsp-managed-mode-hook
+          (lambda ()
+            (when (derived-mode-p 'sh-mode)
+              (setq my/flycheck-local-cache '((lsp . ((next-checkers . (sh-shellcheck)))))))))
